ae4c8e547887dc2843acaf8ad87212b624869f09
Henrique Gaiardoni <167096110+henriquegsantanna@users.noreply.github.com>
2025-07-25 12:21:48 -0300

    Funcao cadastro

---
+++ main.py
@@ -0,0 +1,28 @@
+def Cadastro():
+    while True:
+        usuario = input("Digite seu nome de usuario: ")
+        senha = input("Digite sua senha: ")
+        arquivo = open('usuarios.txt', 'r')
+        
+        if usuario in arquivo:
+            print("Usuario existente.")
+            continue
+        else:
+            tarefasFazer = []
+            tarefasFeitas = []
+            tarefasExcluidas = []
+            arquivo.write(' ' ' ' ' ' ' ' ' ', usuario, senha, tarefasFazer, tarefasFeitas, tarefasExcluidas)
+            print("Usuario cadastrado!")
+            Menu()
+
+#while True:
+    print("Bem-Vindo!")
+    print("1 - Cadastrar \n2 - Entrar")
+    opcao = input("Selecione uma opcao: ")
+    if opcao == 1:
+        Cadastro()
+    elif opcao == 2:
+        Login()
+    else:
+        print("Selecione uma opcao valida.")
+        continue
